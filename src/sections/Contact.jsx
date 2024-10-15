101c729e8b02b84fa8523803d45a0fab9c3b906d
hanmpark <hanmin.01.park@gmail.com>
2024-10-15 19:56:15 +0200

    Template done

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,110 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+	const formRef = useRef();
+	const [loading, setLoading] = useState(false);
+	const [form, setForm] = useState({
+		name: "",
+		email: "",
+		message: ""
+	});
+
+	const handleChange = ({ target: { name, value }}) => {
+		setForm({ ...form, [name]: value });
+	}
+
+	// service_4133enl
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+
+		try {
+			await emailjs.send(
+				"service_4133enl",
+				"template_qmyz1om",
+				{
+					from_name: form.name,
+					to_name: 'Hanmin',
+					from_email: form.email,
+					to_email: 'hanmin.01.park@gmail.com',
+					message: form.message
+				},
+				"1TY4_hEDfEJK1VZ1P"
+			);
+			setLoading(false);
+			alert("Message sent successfully!");
+
+			setForm({
+				name: "",
+				email: "",
+				message: ""
+			});
+		} catch (error) {
+			setLoading(false);
+			console.error(error);
+			alert("Something went wrong!");
+		}
+
+	}
+
+	return (
+		<section className="c-space my-20">
+			<div className="relative min-h-screen flex items-center justify-center flex-col">
+				<img src="/assets/terminal.png" alt="terminal" className="absolute inset-0 min-h-screen"/>
+				<div className="contact-container">
+					<h3>Let's talk</h3>
+					<p className="text-lg text-white-600 mt-3">Whether you are looking to build a new website, improve your existing platform, or bring a unique project to life, I'm here to help.</p>
+
+					<form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
+						<label className="space-y-3">
+							<span className="field-label">Full Name</span>
+							<input
+								type="text"
+								name="name"
+								value={form.name}
+								onChange={handleChange}
+								required
+								className="field-input"
+								placeholder="John Doe"
+							/>
+						</label>
+						<label className="space-y-3">
+							<span className="field-label">Email</span>
+							<input
+								type="email"
+								name="email"
+								value={form.email}
+								onChange={handleChange}
+								required
+								className="field-input"
+								placeholder="johndoe@gmail.com"
+							/>
+						</label>
+						<label className="space-y-3">
+							<span className="field-label">Your message</span>
+							<textarea
+								name="message"
+								value={form.message}
+								onChange={handleChange}
+								required
+								rows={5}
+								className="field-input"
+								placeholder="Hi, I'm interested in..."
+							/>
+						</label>
+						<button className="field-btn" type="submit" disabled={loading}>
+							{loading ? "Sending..." : "Send Message"}
+
+							<img src="/assets/arrow-up.png" alt="arrow-up" className="field-btn_arrow"/>
+						</button>
+					</form>
+				</div>
+			</div>
+			<h3 className="head-text">Contact Me</h3>
+		</section>
+	)
+}
+
+export default Contact;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
 
 	return (
 		<section className="min-h-screen w-full flex flex-co relative" id="home">
-			<div className="w-full mx-auto flex flex-col sm:mt-36 m-20 c-space gap-3">
+			<div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
 				<p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Hanmin <span className="waving-hand">👋</span></p>
 				<p className="hero_tag text-gray_gradient">Building Products & Brands</p>
 			</div>
@@ -29,12 +29,12 @@ const Hero = () => {
 			<div className="w-full h-full absolute inset-0">
 				<Canvas className="w-full h-full">
 					<Suspense fallback={<CanvasLoader/>}>
-						<PerspectiveCamera makeDefault position={[0, 0, 30]}/>
+						<PerspectiveCamera makeDefault position={[0, 0, 20]}/>
 						<HeroCamera isMobile={isMobile}>
 							<HackerRoom
 								position={sizes.deskPosition}
-								rotation={[0, -Math.PI, 0]}
 								scale={sizes.deskScale}
+								rotation={[0, -Math.PI, 0]}
 							/>
 						</HeroCamera>
 						<group>
@@ -49,7 +49,7 @@ const Hero = () => {
 				</Canvas>
 			</div>
 			<div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
-				<a href="#contact" className="w-fit">
+				<a href="#about" className="w-fit">
 					<Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96"/>
 				</a>
 			</div>
