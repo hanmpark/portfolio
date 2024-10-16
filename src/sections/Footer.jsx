6e3cede97992ec81bc9f737f8bb0ef368abe092c
hanmpark <hanmin.01.park@gmail.com>
2024-10-16 18:41:06 +0200

    Footer: Social media implementation done

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,12 +8,15 @@ const Footer = () => {
 			</div>
 
 			<div className="flex gap-3">
-				<div className="social-icon">
+				<a className="social-icon hover:cursor-pointer hover:bg-black-500 transition-all" href="https://github.com/hanmpark" target="_blank" rel="noreferrer">
 					<img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-				</div>
-				<div className="social-icon">
+				</a>
+				<a className="social-icon hover:cursor-pointer hover:bg-black-500 transition-all" href="https://www.instagram.com/hanmin.prk" target="_blank" rel="noreferrer">
 					<img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-				</div>
+				</a>
+				<a className="social-icon hover:cursor-pointer hover:bg-black-500 transition-all" href="https://www.linkedin.com/in/hanmin-park-83239718b/" target="_blank" rel="noreferrer">
+					<img src="/assets/linkedinlogo.png" alt="instagram" className="w-1/2 h-1/2" />
+				</a>
 			</div>
 			<p className="text-white-500">© 2024 Hanmin. All rights reserved.</p>
 		</section>
