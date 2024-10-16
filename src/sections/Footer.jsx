fcb0b77b4a738d61929229f77ce342f6b92bdd4a
Hanmin <hanmin.01.park@gmail.com>
2024-10-16 23:10:22 +0200

    Core: Added tab icon

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,9 +5,12 @@ const Footer = () => {
 
 	return (
 		<section className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5 relative min-h-[120px]">
-			<p className="hover:cursor-pointer text-white-500" onClick={() => navigate("/privacy-policy")}>Privacy Policy</p>
+			<div className="flex gap-4 mr-20">
+				<p className="text-white-500">Hanmin</p>
+				<p className="hover:cursor-pointer text-white-500" onClick={() => navigate("/privacy-policy")}>Privacy Policy</p>
+			</div>
 
-			<div className="flex gap-3 absolute left-1/2 transform -translate-x-1/2">
+			<div className="flex gap-3">
 				<a className="social-icon hover:cursor-pointer hover:bg-black-500 transition-all" href="https://github.com/hanmpark" target="_blank" rel="noreferrer">
 					<img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
 				</a>
